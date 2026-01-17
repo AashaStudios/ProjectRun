4b159cdbc9c3b20a3427c3f793457259b3c7ae48
Vespine <142816007+VespineSpring@users.noreply.github.com>
2026-01-17 13:03:42 +0530

    Game Fix #1 is Done!

--- scenes/slime.gd
+++ scenes/slime.gd
@@ -5,6 +5,7 @@ const SPEED: int = 50
 var direction: int = 1
 
 
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var ray_cast_right: RayCast2D = $RayCastRight
 @onready var ray_cast_left: RayCast2D = $RayCastLeft
 
@@ -12,7 +13,10 @@ var direction: int = 1
 func _process(delta: float) -> void:
     if ray_cast_left.is_colliding():
         direction = 1
+        sprite.flip_h = false
+        
     if ray_cast_right.is_colliding():
         direction = -1
+        sprite.flip_h = true
         
     position.x += direction * SPEED * delta
