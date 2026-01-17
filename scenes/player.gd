24858753f2d5c972d11229c48e047788f6be0faa
Vespine <142816007+VespineSpring@users.noreply.github.com>
2026-01-17 16:08:29 +0530

    Shifted each process in functions (player.gd)

--- scenes/player.gd
+++ scenes/player.gd
@@ -5,6 +5,7 @@ const MAX_SPEED: int = 200
 const ACCELERATION: int = 17
 const FRICTION: int = 20
 const JUMP_VELOCITY: int = -275
+const DASH_VELOCITY: int = 500
 
 var coyote_time_activated: bool = false
 
@@ -14,13 +15,29 @@ var coyote_time_activated: bool = false
 
 
 func _physics_process(delta: float) -> void:
-    # Jump Code
+    if not is_on_floor():
+        velocity += get_gravity() * delta
+        
+    var direction: float = Input.get_axis("left", "right")
+    
+    handle_running(delta, direction)
+    handle_jump()
+    handle_animation(direction)
+
+    move_and_slide()
+
+
+func handle_running(delta: float, direction: float) -> void:
+    var velocity_weight: float = delta * (ACCELERATION if direction else FRICTION)
+    velocity.x = lerp(velocity.x, direction * MAX_SPEED, velocity_weight)
+
+
+func handle_jump() -> void:
     if is_on_floor():
         coyote_time_activated = false
     else:
         if coyote_timer.is_stopped() and !coyote_time_activated:
             coyote_timer.start()
-            print(coyote_timer.time_left)
             coyote_time_activated = true
         
         if Input.is_action_just_released("jump") or is_on_ceiling():
@@ -36,16 +53,8 @@ func _physics_process(delta: float) -> void:
         coyote_timer.stop()
         coyote_time_activated = true
 
-    # Gravity Code
-    if not is_on_floor():
-        velocity += get_gravity() * delta
 
-    # Running Code
-    var direction: float = Input.get_axis("left", "right")
-    var velocity_weight: float = delta * (ACCELERATION if direction else FRICTION)
-    
-    velocity.x = lerp(velocity.x, direction * MAX_SPEED, velocity_weight)
-    
+func handle_animation(direction: float) -> void:
     if direction > 0:
         sprite.flip_h = false
     elif direction < 0:
@@ -58,5 +67,3 @@ func _physics_process(delta: float) -> void:
             sprite.play("run")
     else:
         sprite.play("jump")
-
-    move_and_slide()
