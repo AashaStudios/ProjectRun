f4523bf99d3a8b81b4a8afdb91724816b7e821ad
Vespine <142816007+VespineSpring@users.noreply.github.com>
2026-01-17 14:49:05 +0530

    Smooth Movement

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,10 +1,14 @@
 extends CharacterBody2D
 
 
-const SPEED = 150
-const JUMP_VELOCITY = -250
+const MAX_SPEED: int = 200
+const ACCELERATION: int = 15
+const FRICTION: int = 12
+const JUMP_VELOCITY: int = -250
 
 @onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var coyote_timer: Timer = $CoyoteTimer
+@onready var jump_buffer_timer: Timer = $JumpBufferTimer
 
 
 func _physics_process(delta: float) -> void:
@@ -14,12 +18,10 @@ func _physics_process(delta: float) -> void:
     if Input.is_action_pressed("jump") and is_on_floor():
         velocity.y = JUMP_VELOCITY
 
-    var direction := Input.get_axis("left", "right")
+    var direction: float = Input.get_axis("left", "right")
+    var velocity_weight: float = delta * (ACCELERATION if direction else FRICTION)
     
-    if direction:
-        velocity.x = direction * SPEED
-    else:
-        velocity.x = move_toward(velocity.x, 0, SPEED)
+    velocity.x = lerp(velocity.x, direction * MAX_SPEED, velocity_weight)
     
     if direction > 0:
         sprite.flip_h = false
