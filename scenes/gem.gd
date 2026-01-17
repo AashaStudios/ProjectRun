3dd0c0a8fbf306e5846dce651c358c27c73cea26
Vespine <142816007+VespineSpring@users.noreply.github.com>
2026-01-17 19:07:18 +0530

    Animated Gems are Now Available!

--- scenes/gem.gd
+++ scenes/gem.gd
@@ -1,6 +1,13 @@
 extends Area2D
 
 
+func _ready() -> void:
+    var tween := create_tween()
+    tween.set_loops()
+    tween.tween_property($"Sprite2D", "position", Vector2(0, -4), 0.75).set_trans(Tween.TRANS_CUBIC)
+    tween.tween_property($"Sprite2D", "position", Vector2(0, 0), 0.75).set_trans(Tween.TRANS_CUBIC)
+
+
 func _on_body_entered(_body: Node2D) -> void:
     print("+1 Gem")
     queue_free()
