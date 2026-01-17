5a4574da8e55ecbeed9f0746b59ff41479f72fc3
Vespine <142816007+VespineSpring@users.noreply.github.com>
2026-01-17 15:07:15 +0530

    Variable Jump, Jump Buffering, Coyote Time Done!

--- scenes/player.gd
+++ scenes/player.gd
@@ -2,9 +2,11 @@ extends CharacterBody2D
 
 
 const MAX_SPEED: int = 200
-const ACCELERATION: int = 15
-const FRICTION: int = 12
-const JUMP_VELOCITY: int = -250
+const ACCELERATION: int = 17
+const FRICTION: int = 20
+const JUMP_VELOCITY: int = -275
+
+var coyote_time_activated: bool = false
 
 @onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var coyote_timer: Timer = $CoyoteTimer
@@ -12,12 +14,33 @@ const JUMP_VELOCITY: int = -250
 
 
 func _physics_process(delta: float) -> void:
+    # Jump Code
+    if is_on_floor():
+        coyote_time_activated = false
+    else:
+        if coyote_timer.is_stopped() and !coyote_time_activated:
+            coyote_timer.start()
+            print(coyote_timer.time_left)
+            coyote_time_activated = true
+        
+        if Input.is_action_just_released("jump") or is_on_ceiling():
+            velocity.y *= 0.5
+    
+    if Input.is_action_pressed("jump"):
+        if jump_buffer_timer.is_stopped():
+            jump_buffer_timer.start()
+    
+    if !jump_buffer_timer.is_stopped() and (!coyote_timer.is_stopped() or is_on_floor()):
+        velocity.y = JUMP_VELOCITY
+        jump_buffer_timer.stop()
+        coyote_timer.stop()
+        coyote_time_activated = true
+
+    # Gravity Code
     if not is_on_floor():
         velocity += get_gravity() * delta
 
-    if Input.is_action_pressed("jump") and is_on_floor():
-        velocity.y = JUMP_VELOCITY
-
+    # Running Code
     var direction: float = Input.get_axis("left", "right")
     var velocity_weight: float = delta * (ACCELERATION if direction else FRICTION)
     
