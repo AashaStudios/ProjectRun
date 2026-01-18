06241fde05834c62a6b67c4b53e3c54a5b24af0d
Vespine <142816007+VespineSpring@users.noreply.github.com>
2026-01-18 16:41:58 +0530

    Added Coin/Gem SFX #16

--- scenes/coin.gd
+++ scenes/coin.gd
@@ -7,4 +7,13 @@ extends Area2D
 func _on_body_entered(_body: Node2D) -> void:
     game_manager.coins += 1
     print("Coins: " + str(game_manager.coins))
-    queue_free()
+    
+    var pitch_value: float = randf_range(0.9, 1.1)
+    
+    var pickup_tween = create_tween()
+    pickup_tween.tween_property($AnimatedSprite2D, "visible", false, 0)
+    pickup_tween.tween_property($CollisionShape2D, "disabled", true, 0)
+    pickup_tween.tween_property($AudioStreamPlayer2D, "pitch_scale", pitch_value, 0)
+    pickup_tween.tween_property($AudioStreamPlayer2D, "playing", true, 0)
+    pickup_tween.tween_interval(1)
+    pickup_tween.tween_callback($".".queue_free)
--- scenes/gem.gd
+++ scenes/gem.gd
@@ -7,11 +7,20 @@ extends Area2D
 func _ready() -> void:
     var tween := create_tween()
     tween.set_loops()
-    tween.tween_property($"Sprite2D", "position", Vector2(0, -4), 0.75).set_trans(Tween.TRANS_CUBIC)
-    tween.tween_property($"Sprite2D", "position", Vector2(0, 0), 0.75).set_trans(Tween.TRANS_CUBIC)
+    tween.tween_property($"Sprite2D", "position", Vector2(0, -4), 0.5).set_trans(Tween.TRANS_CUBIC)
+    tween.tween_property($"Sprite2D", "position", Vector2(0, 0), 0.5).set_trans(Tween.TRANS_CUBIC)
 
 
 func _on_body_entered(_body: Node2D) -> void:
     game_manager.gems += 1
     print("Gems: " + str(game_manager.gems))
-    queue_free()
+    
+    var pitch_value: float = randf_range(0.9, 1.1)
+    
+    var pickup_tween = create_tween()
+    pickup_tween.tween_property($Sprite2D, "visible", false, 0)
+    pickup_tween.tween_property($CollisionShape2D, "disabled", true, 0)
+    pickup_tween.tween_property($AudioStreamPlayer2D, "pitch_scale", pitch_value, 0)
+    pickup_tween.tween_property($AudioStreamPlayer2D, "playing", true, 0)
+    pickup_tween.tween_interval(1)
+    pickup_tween.tween_callback($".".queue_free)
