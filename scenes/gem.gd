2e85ba160288102d1dcd92cd777686bdac7dce9b
Vespine <142816007+VespineSpring@users.noreply.github.com>
2026-01-19 23:28:03 +0530

    Created level 1!

--- scenes/gem.gd
+++ scenes/gem.gd
@@ -7,8 +7,8 @@ extends Area2D
 func _ready() -> void:
     var tween := create_tween()
     tween.set_loops()
-    tween.tween_property($"Sprite2D", "position", Vector2(0, -4), 0.5).set_trans(Tween.TRANS_CUBIC)
-    tween.tween_property($"Sprite2D", "position", Vector2(0, 0), 0.5).set_trans(Tween.TRANS_CUBIC)
+    tween.tween_property($"Sprite2D", "position", Vector2(0, -4), 0.75).set_trans(Tween.TRANS_CUBIC)
+    tween.tween_property($"Sprite2D", "position", Vector2(0, 0), 0.75).set_trans(Tween.TRANS_CUBIC)
 
 
 func _on_body_entered(_body: Node2D) -> void:
