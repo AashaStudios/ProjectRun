c2f229f2fb4bee9095900c87dc1c3b19031a5d3f
Vespine <142816007+VespineSpring@users.noreply.github.com>
2026-01-23 12:29:39 +0530

    Made a basic stopwatch

--- scenes/game_hud.gd
+++ scenes/game_hud.gd
@@ -4,8 +4,19 @@ extends Control
 @onready var game_manager: Node = %GameManager
 @onready var coin_count_label: Label = $Coins/CoinCountLabel
 @onready var gem_count_label: Label = $Gems/GemCountLabel
+@onready var stopwatch_label: Label = $StopwatchLabel
 
 
 func _process(_delta: float) -> void:
     coin_count_label.text = " x " + str(game_manager.coins)
     gem_count_label.text = " x " + str(game_manager.gems)
+    
+    update_time(game_manager.stopwatch_time)
+
+
+func update_time(time_in_seconds: float) -> void:
+    var minutes: int = int(time_in_seconds / 60) % 60
+    var seconds: int = int(time_in_seconds) % 60
+    var milliseconds: int = int(time_in_seconds * 1000.0) % 1000
+    
+    stopwatch_label.text = "%02d:%02d.%01d" % [minutes, seconds, milliseconds]
--- scenes/game_manager.gd
+++ scenes/game_manager.gd
@@ -3,7 +3,12 @@ extends Node
 
 var coins: int = 0
 var gems: int = 0
+var stopwatch_time: float = 0
 
 
 func _ready() -> void:
     RenderingServer.set_default_clear_color(Color(0.53, 0.81, 0.92))
+
+
+func _process(delta: float) -> void:
+    stopwatch_time += delta
