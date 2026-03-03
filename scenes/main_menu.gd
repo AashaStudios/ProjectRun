9aebf1dc4f2fae451d79d567581cf004519400b9
Vespine <142816007+VespineSpring@users.noreply.github.com>
2026-03-03 15:51:54 +0530

    Fixed Buttons!

--- scenes/main_menu.gd
+++ scenes/main_menu.gd
@@ -2,8 +2,7 @@ extends Control
 
 
 func _on_play_button_pressed() -> void:
-    #SceneTransition.change_scene_to("res://scenes/game.tscn")
-    pass
+    SceneTransition.change_scene_to("res://scenes/game.tscn")
 
 
 func _on_settings_button_pressed() -> void:
